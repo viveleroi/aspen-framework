93b4dd5dd1c75b8b037881834d48c39585246174
Michael Botsko <botsko@gmail.com>
2009-05-05 10:48:38 -0700

    fixed module include'

--- system/config.default.php
+++ system/config.default.php
@@ -324,6 +324,7 @@
 	$config['load_core_class'][] = array('classname' => 'Install');
 	$config['load_core_class'][] = array('classname' => 'Log');
 	$config['load_core_class'][] = array('classname' => 'Model');
+	$config['load_core_class'][] = array('classname' => 'Module');
 	$config['load_core_class'][] = array('classname' => 'Modules');
 	$config['load_core_class'][] = array('classname' => 'PHPMailer', 'var' => 'mail');
 	$config['load_core_class'][] = array('classname' => 'Settings');
