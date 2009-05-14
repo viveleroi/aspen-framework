0b4b46b59133222edeee3ee7f5d9fe1db6f05b61
Michael Botsko <botsko@gmail.com>
2009-05-13 17:43:46 -0700

    removed accidental add

--- build/aspen-framework/admin/templates/database-error.tpl.php
+++
@@ -1,4 +0,0 @@
-<h2><?php print $this->text('dberror:title'); ?></h2>
-
-
-<p><?php print $this->text('dberror:message'); ?></p>
--- build/aspen-framework/admin/templates/footer.tpl.php
+++
@@ -1,7 +0,0 @@
-	
-			</div>
-			<hr />
-			<p class="small"><a href="http://www.aspen-framework.org"><?php print $this->APP->config('application_name'); ?></a> <?php print VERSION ?> &mdash; <em><?php print $this->text('copyright'); ?></em></p>
-		</div>
-	</body>
-</html>
