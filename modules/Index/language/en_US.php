795a274f1ca3ad0f3cdf9b088733467dd3839033
Michael Botsko <botsko@gmail.com>
2010-10-29 09:16:38 -0700

    more cleanup

--- modules/Index/language/en_US.php
+++ modules/Index/language/en_US.php
@@ -1,5 +1,5 @@
 <?php
 $lang['admin']['index:view:head-title'] = 'Welcome';
-$lang['admin']['index:view:page-title'] = 'Welcome!';
-$lang['admin']['index:view:intro'] = '<p><em>This template is located in <code>modules/Index/templates_admin/view.php</code></em>.</p>';
+$lang['admin']['index:view:page-title'] = 'Welcome';
+$lang['admin']['index:view:intro'] = 'This template is located in <code>modules/Index/templates_admin/view.php</code>';
 ?>
--- modules/Index/templates_admin/view.tpl.php
+++ modules/Index/templates_admin/view.tpl.php
@@ -1,3 +1,3 @@
 <h2><?= text('index:view:page-title'); ?></h2>
 <?= app()->sml->printMessage(); ?>
-<?= text('index:view:intro'); ?>
+<p><em><?= text('index:view:intro'); ?></em></p>
