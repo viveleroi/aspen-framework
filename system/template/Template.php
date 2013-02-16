07af328c3865b18b2e7dc0cf69eeab96cad46159
Mike Botsko <botsko@gmail.com>
2013-02-16 07:55:59 -0800

    Fixed a template file regression

--- system/template/Template.php
+++ system/template/Template.php
@@ -34,13 +34,13 @@ function text(){
  * @package Aspen_Framework
  */
 class Template  {
-	
+
 	/**
 	 * @var string Holds the page template name
 	 * @access public
 	 */
 	public $page;
-	
+
 	/**
 	 * @var string Holds the layout template name
 	 * @access public
@@ -88,14 +88,14 @@ class Template  {
 	 * @access private
 	 */
 	private $terms;
-	
+
 	/**
 	 * Holds the current template file path
-	 * @var type 
+	 * @var type
 	 */
 	private $template;
-	
-	
+
+
 	/**
 	 * Returns the layouts directory
 	 * @return string
@@ -117,30 +117,30 @@ class Template  {
 		$module = router()->cleanModule($module);
         return router()->getModulePath($module, $orig_interface) . DS . 'templates' . ($interface == '' ? false : '_' . $interface);
     }
-	
-	
+
+
 	/**
 	 * This method allows you to add resources to the page. Every resource
 	 * should be passed here, as an appropriate object. The template will
 	 * print/use the resource depending its type.
 	 */
 	public function add_resource( $resource ){
-		
+
 		if( is_object($resource) ){
-			
+
 			// Handle javascript objects by printing their script elements
 			if( $resource instanceof Aspen_JavaScript){
 				$this->_load_js[] = $resource;
 			}
-			
+
 			// Handle css objects by printing their link/style elements
 			if( $resource instanceof Aspen_Css){
 				$this->_load_css[] = $resource;
 			}
 		}
 	}
-	
-	
+
+
 	/**
 	 * Allows the user to add custom styles which will be printed with module header
 	 * @param string $selector
@@ -150,8 +150,8 @@ class Template  {
 	public function setCssStyle($selector, $attr, $value){
 		$this->_css_styles[] = array('selector'=>$selector,'attr'=>$attr,'value'=>$value);
 	}
-	
-	
+
+
 	/**
 	 * Sorts and re-arranges css/javascript includes
 	 */
@@ -171,9 +171,9 @@ class Template  {
 	 * @access public
 	 */
 	public function loadModuleHeader(){
-		
+
 		$this->prepareMediaIncludes();
-		
+
 		// append any css files for loading
 		if(!empty($this->_load_css)){
 			foreach($this->_load_css as $css){
@@ -211,8 +211,8 @@ class Template  {
 			}
 		}
 	}
-	
-	
+
+
 	/**
 	 * Loads a header file for the currently loaded module, if that file exists
 	 * @access public
@@ -249,7 +249,7 @@ class Template  {
 	private function staticUrl($args){
 
 		$basepath = $filename = '';
-		
+
 		if($args['url']){
 			$file = $args['url'];
 		}
@@ -268,12 +268,12 @@ class Template  {
 		return $file;
 
 	}
-	
-	
+
+
 	/**
 	 *
 	 * @param type $path
-	 * @return type 
+	 * @return type
 	 */
 	public function parseMediaFilePath($path){
 		$base = array();
@@ -335,19 +335,19 @@ class Template  {
 			$this->_data = array_merge($this->_data, $data);
 		}
 	}
-	
-	
+
+
 	/**
 	 * Display all templates that have been primed for output
 	 * @param $data Array of data to be passed
 	 * @access public
 	 */
 	public function page(){
-		
+
 		if(!$this->template){
 			$this->setPage();
 		}
-		
+
 		if(file_exists($this->template) && strpos($this->template, APPLICATION_PATH) !== false){
 			// pass through variables
 			if(is_array($this->_data)){
@@ -374,7 +374,7 @@ class Template  {
 		if(config()->get('require_form_token_auth')){
 			$token = app()->security->generateFormToken();
 		}
-		
+
 		$layout = $this->getLayoutDir().DS.$this->layout.'.tpl.php';
 
 		if(file_exists($layout)){
@@ -390,27 +390,27 @@ class Template  {
 			include($layout);
 
 		}
-		
+
 		$this->resetTemplateQueue();
 
 	}
-	
-	
+
+
 	/**
 	 * Set the current layout.
-	 * @param string $layout 
+	 * @param string $layout
 	 */
 	public function setLayout($layout){
 		$this->layout = $layout;
 	}
-	
-	
+
+
 	/**
 	 * Set the current page template.
-	 * @param string $page 
+	 * @param string $page
 	 */
 	public function setPage( $page = false, $module = false ){
-		
+
 		$this->page = $page;
 		$page = $this->page ? $this->page : router()->method();
 		if(router()->method() == 'add'){
@@ -419,7 +419,7 @@ class Template  {
 			}
 		}
 		$this->template = $this->getModuleTemplateDir( $module ).DS.$page.'.tpl.php';
-		
+
 	}
 
 
@@ -444,14 +444,14 @@ class Template  {
 	public function at($path = false, $type = 'method'){
 		return (router()->here($path,$type) ? ' class="'.config()->get('active_link_class_name').'"' : '');
 	}
-	
-	
+
+
 	/**
 	 * Generates an interface/module/method path string
 	 * @param string $method
 	 * @param string $module
 	 * @param string $interface
-	 * @return string 
+	 * @return string
 	 */
 	public function getNamespacePath($method, $module = false, $interface = false){
 		$path = '';
@@ -504,101 +504,12 @@ class Template  {
 
 
 	/**
-	 * Creates an xhtml valid url to the current page with params, replacing any existing params
-	 * @param array $params
-	 * @param string $method
-	 * @return string
-	 * @access public
-	 */
-	public function xhtmlSelfUrl($bits = false, $path = false){
-		$new_params = router()->getMappedArguments();
-		// remove any options from the url that are in our new params
-		if(is_array($bits) && count($bits)){
-			foreach($bits as $key => $value){
-				$new_params[$key] = $value;
-			}
-		}
-		if(!$path){
-			$map = router()->getOriginalMap();
-			$path = $map['module'].'/'.$map['method'];
-		}
-		return $this->xhtmlUrl($path, $new_params);
-	}
-
-
-	/**
-	 * Creates a url to the current page with params, replacing any existing params
-	 * @param array $params
-	 * @param string $method
-	 * @return string
-	 * @access public
-	 */
-	public function selfUrl($bits = false, $path = false){
-		$new_params = router()->getMappedArguments();
-		// remove any options from the url that are in our new params
-		if(is_array($bits) && count($bits)){
-			foreach($bits as $key => $value){
-				$new_params[$key] = $value;
-			}
-		}
-		if(!$path){
-			$map = router()->getOriginalMap();
-			$path = $map['module'].'/'.$map['method'];
-		}
-		return Url::path($path, $new_params);
-	}
-
-
-	/**
-	 * Creates a link for sorting a result set
-	 * @param string $title
-	 * @param string $location
-	 * @param string $sort_by
-	 * @return string
-	 */
-	public function sortLink($title, $location, $sort_by, $add_class = false){
-
-		$base = $this->xhtmlSelfUrl();
-		$sort = app()->prefs->getSort($location, $sort_by);
-
-		// determine the sort direction
-		$new_direction = $sort['sort_direction'] == "ASC" ? "DESC" : "ASC";
-		
-		// add class
-		$add_class = $add_class ? ' '.$add_class : '';
-
-		// determine the proper class, if any
-		$class = 'sortable';
-		if($sort['sort_by'] == $sort_by){
-			$class = strtolower($new_direction);
-		}
-
-		// build proper url
-		$url = $base.'?'.http_build_query(array(
-									'sort_location'=>$location,
-									'sort_by'=>$sort_by,
-									'sort_direction'=>$new_direction), '', '&amp;');
-
-		// create the link
-		$html = sprintf('<a href="%s" title="%s" class="%s">%s</a>',
-								$url,
-								'Sort ' . Link::encodeTextEntities($title) . ' column ' . ($new_direction == 'ASC' ? 'ascending' : 'descending'),
-								$class.$add_class,
-								Link::encodeTextEntities($title)
-							);
-
-		return $html;
-
-	}
-
-
-	/**
 	 * Generate a basic LI set of pagination links
 	 * @param array $pages
 	 * @return string
 	 */
 	public function paginateLinks($pages){
-		
+
 		$current_page	= $pages['current_page'];
 		$per_page		= $pages['per_page'];
 		$total_pages	= $pages['total_pages'];
@@ -723,7 +634,7 @@ class Template  {
 
 	}
 
-	
+
 	/**
 	 * Returns a specific filter value from the GET params
 	 * @param  string $key The key of the filter value you want
@@ -798,7 +709,7 @@ class Template  {
 		// the database
 		if($method == "ENUM"){
 			$schema = app()->getDatabaseSchema($selectTable);
-			foreach($my_enums as $value){
+			foreach($schema['schema'] as $value){
 				if($value->name == $selectField){
 					foreach($value->enums as $choice){
 						$choice = str_replace("'", "", $choice);
